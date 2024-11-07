a49019d8ee65e2d00bef2f2c43ada7bf722ed939
Abdul Wadood <abdul.wadood@inapp.com>
2024-11-07 14:32:08 +0530

    Remove secrets

--- src/movie.service.js
+++ src/movie.service.js
@@ -2,7 +2,6 @@ export function getPopularMovies() {
     return fetch('https://api.themoviedb.org/3/movie/popular', {
         method: 'GET',
         headers: {
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMTAxNGY5OGMzNjI0NjhlY2Q2Yzk4ZGM2ZDZiNDBhNSIsIm5iZiI6MTczMDkyMzk3MC4yODMyNzk0LCJzdWIiOiI1OWMzMzU1NzkyNTE0MTViMzIwMDgyMmIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.QTGNk90Su7pXRp6kn8joK2n2n_lyiHhW6qlnHRD8uoQ'
         }
     })
     .then(res => res.json());
